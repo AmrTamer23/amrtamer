9cdd9c5732325ea4de082bf357abf720608b18d1
AmrTamer23 <amr12300062@cs.modern-academy.edu.eg>
2024-02-29 21:43:30 +0200

    setup NudgeerSafe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+import NudgeerSafe from "@onboardbase/nudgeer-safe";
+
+const headers = new NudgeerSafe().astro();
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), vue()],
+  headers,
   vite: {
     ssr: {
       noExternal: ["react-icons"],
     },
   },
+  adapter: node({
+    mode: "standalone",
+  }),
+  output: "server",
 });
