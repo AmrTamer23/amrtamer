94c85183f9757caa5b379b685ec7c480273354b9
AmrTamer23 <amr12300062@cs.modern-academy.edu.eg>
2024-03-18 07:29:46 +0200

    Edit Astro conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,9 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-import NudgeerSafe from "@onboardbase/nudgeer-safe";
-import vercel from "@astrojs/vercel/serverless";
-const headers = new NudgeerSafe().astro();
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), vue()],
-
-  vite: {
-    ssr: {
-      noExternal: ["react-icons"],
-    },
-  },
-  adapter: vercel(),
-  output: "server",
 });
