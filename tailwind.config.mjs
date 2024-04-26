c3cadb958ff39618eb92eff183977db0cc8a4801
AmrTamer23 <amrtamer23mj@gmail.com>
2024-04-26 15:11:49 +0300

    add shadcn vue

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
+import animate from "tailwindcss-animate";
 import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx,vue}",
+    "./components/**/*.{ts,tsx,vue}",
+    "./app/**/*.{ts,tsx,vue}",
+    "./src/**/*.{ts,tsx,vue}",
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {
       colors: {
         background: "var(--background-color)",
@@ -20,7 +33,21 @@ export default {
       fontFamily: {
         fira: ["Fira Code", "monospace"],
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [typography()],
+  plugins: [animate, typography()],
 };
