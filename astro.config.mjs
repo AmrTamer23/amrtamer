38bb13bc8f384d9f70a799e6f3eca01191b6f215
AmrTamer23 <amrtamer23mj@gmail.com>
2024-06-07 03:25:56 +0300

    chore: added netlifiy adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: "hybrid",
   prefetch: {
-    prefetchAll: true,
+    prefetchAll: true
   },
+  adapter: netlify()
 });
