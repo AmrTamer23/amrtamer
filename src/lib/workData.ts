3ccb6490f7e28fa6d22442da0858763c83057cbb
AmrTamer23 <amrtamer23mj@gmail.com>
2024-06-07 03:20:48 +0300

    chore: change avater

---
+++ src/lib/workData.ts
@@ -0,0 +1,23 @@
+export const workHistory: Work[] = [
+  {
+    location: "North America, Remote ",
+    date: " Jul2023 - Sep2023",
+    role: "Frontend Intern",
+    company: "Cloudypedia",
+    companyLink: "https://cloudypedia.com",
+    description: ` I've joined Cloudypedia as a frontend intern. I've worked on real world
+    project and collected requirements from clients. I've also worked on the
+    design and development sides of the project.`,
+    skills: "HTML, CSS, JavaScript, React, TailwindCSS, Figma.",
+  },
+  {
+    location: "Cairo, Egypt",
+    date: "Apr2024 - Now",
+    role: "Frontend Software Engineer",
+    company: "Brandria",
+    companyLink: "https://dbrandria.com",
+    description:
+      "I've joined Brandria as a frontend software engineer to maintain Salamy also to work and deliver other projects. I've Successfully Shipped 2 projects with satisfying the client's needs.",
+    skills: "JavaScript, TypeScript, TailwindCSS, React, Next.js, WordPress.",
+  },
+];
