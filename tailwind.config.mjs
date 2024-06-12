4dbc53c4a413055073b2ae30aa3be6067d3db180
AmrTamer23 <amrtamer23mj@gmail.com>
2024-06-13 02:59:11 +0300

    feat: added grid background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -60,5 +60,29 @@ export default {
       },
     },
   },
-  plugins: [animate, typography(), addDynamicIconSelectors()],
+  plugins: [
+    animate,
+    typography(),
+    addDynamicIconSelectors(),
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+            )}")`,
+          }),
+        },
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: "color",
+        },
+      );
+    },
+  ],
 };
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+const svgToDataUri = require("mini-svg-data-uri");
