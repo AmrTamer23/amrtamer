1c4d60279679769d676eef9fd896913316264021
AmrTamer23 <amrtamer23mj@gmail.com>
2024-09-06 05:20:00 +0300

    feat: use notion as a headless cms to display dynamic bio

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -21,3 +21,53 @@ type Work = {
   description: string;
   skills: string;
 };
+
+interface NotionUser {
+  object: "user";
+  id: string;
+}
+
+interface NotionTitle {
+  type: "text";
+  text: {
+    content: string;
+    link: null | string;
+  };
+  annotations: {
+    bold: boolean;
+    italic: boolean;
+    strikethrough: boolean;
+    underline: boolean;
+    code: boolean;
+    color: string;
+  };
+  plain_text: string;
+  href: null | string;
+}
+
+interface NotionPage {
+  object: "page";
+  id: string;
+  created_time: string;
+  last_edited_time: string;
+  created_by: NotionUser;
+  last_edited_by: NotionUser;
+  cover: null | any; // You can define a more specific type if needed
+  icon: null | any; // You can define a more specific type if needed
+  parent: {
+    type: "workspace";
+    workspace: boolean;
+  };
+  archived: boolean;
+  in_trash: boolean;
+  properties: {
+    title: {
+      id: string;
+      type: "title";
+      title: NotionTitle[];
+    };
+  };
+  url: string;
+  public_url: string;
+  request_id: string;
+}
