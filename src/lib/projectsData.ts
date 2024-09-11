5fc2f02d469f92878ad5a6a4f5d67d2497f35271
AmrTamer23 <amrtamer23mj@gmail.com>
2024-09-11 20:11:03 +0300

    chore: add streakify to projects

--- src/lib/projectsData.ts
+++ src/lib/projectsData.ts
@@ -14,40 +14,50 @@ export const projects: Project[] = [
     ],
     image:
       "https://res.cloudinary.com/dbiacrzqd/image/upload/f_auto,q_auto/kbhbgp2gupe3pnfncslq",
-    technologies: "ts,tailwindcss,react,nextjs",
+    technologies: ["ts", "tailwindcss", "react", "nextjs"],
     link: "https://salamy.space",
     sourceCode: null,
   },
-  // {
-  //   name: "Quiz Master",
-  //   domain: ".vercel.app",
-  //   description:
-  //     "A quiz platform, Explore quizzes in various genres such as Geography, Computer Science, History, and Sports.",
-  //   features: [
-  //     "Authentication.",
-  //     "Dark/Light Mode.",
-  //     "Difficulty Decision based on points in each genre.",
-  //     "Responsive Design.",
-  //   ],
-  //   image:
-  //     "https://res.cloudinary.com/dbiacrzqd/image/upload/f_auto,q_auto/c8wrcoa2pvy8idrcafwv",
-  //   technologies: "ts,tailwindcss,react,nextjs,firebase",
-  //   link: "https://quizmasterr.vercel.app",
-  //   sourceCode: "https://github.com/AmrTamer23/quizMaster",
-  // },
-  // {
-  //   name: "Tasknote",
-  //   description:
-  //     "a todo and note-taking app. It's designed to effortlessly merge tasks and notes within a single category, saving you valuable time and helping you stay on top of your goals.",
-  //   features: [
-  //     "Store tasks and notes in the local storage",
-  //     "Add, delete tasks and notes",
-  //     "Combine tasks and notes and put them in different tabs in a relevant category",
-  //     "Responsive Design",
-  //   ],
-  //   image: Tasknote.src,
-  //   technologies: "ts,tailwindcss,react",
-  //   link: "https://tasknote-ten.vercel.app",
-  //   sourceCode: "https://github.com/AmrTamer23/tasknote",
-  // },
+  {
+    name: "Streakify",
+    domain: ".netlify.app",
+    description:
+      "Streakify is a dynamic habit-tracking application designed to help users stay consistent with their routines.",
+    fullDescription:
+      "Designed to make habit-building easy and engaging. Users can effortlessly create custom habits, track their consistency, and gain insights through visual data representations. ",
+    features: [
+      "User authentication and management",
+      "Habit creation and tracking",
+      "Progress visualization",
+      "Responsive Design.",
+    ],
+    image: "https://i.ibb.co/sgSdh9W/Streakify.png",
+    technologies: [
+      "ts",
+      "tailwindcss",
+      "vue",
+      "nuxtjs",
+      "supabase",
+      "postgres",
+    ],
+    link: "https://streakify23.netlify.app/",
+    sourceCode: "https://github.com/AmrTamer23/streakify",
+  },
+  {
+    name: "Quiz Master",
+    domain: ".vercel.app",
+    description:
+      "A quiz platform, Explore quizzes in various genres such as Geography, Computer Science, History, and Sports.",
+    features: [
+      "Authentication.",
+      "Dark/Light Mode.",
+      "Difficulty Decision based on points in each genre.",
+      "Responsive Design.",
+    ],
+    image:
+      "https://res.cloudinary.com/dbiacrzqd/image/upload/f_auto,q_auto/c8wrcoa2pvy8idrcafwv",
+    technologies: ["ts", "tailwindcss", "react", "nextjs", "firebase"],
+    link: "https://quizmasterr.vercel.app",
+    sourceCode: "https://github.com/AmrTamer23/quizMaster",
+  },
 ];
