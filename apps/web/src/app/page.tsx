e4b4626b33f14c440add995eda2d8b2f803e3e48
AmrTamer23 <amrtamer23mj@gmail.com>
2025-04-28 04:25:37 +0300

    feat: transition routing

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,68 @@
 "use client";
 import { GlowingEffect } from "@/components/ui/card-glowing-effect";
 import { LinkedinIcon, MailIcon, XIcon } from "@/lib/icons";
+import { useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import CustomEase from "gsap/CustomEase";
+import { useTransitionRouter } from "next-view-transitions";
+
 export default function Home() {
+  const router = useTransitionRouter();
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  function slideInOut() {
+    document.documentElement.animate(
+      [
+        {
+          opacity: 1,
+          transform: "translateY(0)",
+        },
+        {
+          opacity: 0.2,
+          transform: "translateY(-35%)",
+        },
+      ],
+      {
+        duration: 1200,
+        easing: "cubic-bezier(0.87, 0, 0.13, 1)",
+        fill: "forwards",
+        pseudoElement: "::view-transition-old(root)",
+      }
+    );
+
+    document.documentElement.animate(
+      [
+        {
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+        },
+        {
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+        },
+      ],
+      {
+        duration: 1200,
+        easing: "cubic-bezier(0.87, 0, 0.13, 1)",
+        fill: "forwards",
+        pseudoElement: "::view-transition-new(root)",
+      }
+    );
+  }
+
+  const navigateTo = (path: string) => {
+    if (isAnimating) return;
+
+    setIsAnimating(true);
+
+    router.push(path, {
+      onTransitionReady: slideInOut,
+    });
+
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 1500);
+  };
+
   return (
     <main className=" mx-auto flex items-center justify-center px-4 py-2 w-full h-svh">
       <div className="flex gap-4  max-w-2xl relative rounded-2xl border p-6 md:rounded-3xl cursor-default">
@@ -39,8 +100,8 @@ export default function Home() {
           </p>
           <div className="flex w-full justify-end font-mono text-sm underline underline-offset-4 [&_li]:cursor-pointer [&_li]:hover:text-primary [&_li]:after:content-['->']">
             <ul className="flex gap-4">
-              <li>Work History</li>
-              <li>Projects</li>
+              <li onClick={() => navigateTo("/work")}>Work History</li>
+              <li onClick={() => navigateTo("/projects")}>Projects</li>
             </ul>
           </div>
         </div>
